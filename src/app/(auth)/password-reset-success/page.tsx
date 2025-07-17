1864bcd1ddd478e5021ca95fc556343c5c6dd90a
Ale <alex03ortizperez@gmail.com>
2025-07-17 15:57:33 -0600

    fix: buttons message

--- src/app/(auth)/password-reset-success/page.tsx
+++ src/app/(auth)/password-reset-success/page.tsx
@@ -31,7 +31,7 @@ const PasswordResetSuccessPage = () => {
                             <path
                                 fillRule="evenodd"
                                 clipRule="evenodd"
-                                d="M4.755 10.059a7.5 7.5 0 0 1 12.548-3.364l1.903 1.903h-3.183a.75.75 0 1 0 0 1.5h4.992a.75.75 0 0 0 .75-.75V4.356a.75.75 0 0 0-1.5 0v3.18l-1.9-1.9A9 9 0 0 0 3.306 9.67a.75.75 0 1 0 1.45.388Zm15.408 3.352a.75.75 0 0 0-.919.53 7.5 7.5 0 0 1-12.548 3.364l-1.902-1.903h3.183a.75.75 0 0 0 0-1.5H2.984a.75.75 0 0 0-.75.75v4.992a.75.75 0 0 0 1.5 0v-3.18l1.9 1.9a9 9 0 0 0 15.059-4.035.75.75 0 0 0-.53-.918Z"/>
+                                d="M4.755 10.059a7.5 7.5 0 0 1 12.548-3.364l1.903 1.903h-3.183a.75.75 0 1 0 0 1.5h4.992a.75.75 0 0 0 .75-.75V4.356a.75.75 0 0 0-1.5 0v3.18l-1.9-1.9A9 9 0 0 0 3.306 9.67a.75.75 0 1 0 1.45.388Zm15.408 3.352a.75.75 0 0 0-.919.53 7.5 7.5 0 0 1-12.548 3.364l-1.902-1.903h3.183a.75.75 0 0 0 0-1.5H2.984a.75.75 0 0 0-.75.75v4.992a.75.75 0 0 0 1.5 0v-3.18l1.9 1.9a9 9 0 0 0 15.059-4.035.75.75 0 0 0-.53-.918Z" />
                         </svg>
                     </div>
 
@@ -45,7 +45,7 @@ const PasswordResetSuccessPage = () => {
                         className="h-11 bg-[hsl(var(--accent))] rounded-full flex items-center justify-center px-4 py-2.5 hover:bg-opacity-90 transition-all focus:outline-none focus:ring-2 focus:ring-[hsl(var(--accent))] focus:ring-offset-2 focus:ring-offset-[hsl(var(--dark))] mx-auto"
                         aria-label="Solicitar código de restablecimiento de contraseña">
                         <Body className="font-bold text-base text-white">
-                            Solicitar código de restablecimiento
+                            Inicio de sesión
                         </Body>
                     </button>
 
