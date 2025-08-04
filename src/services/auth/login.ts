94923cdb136284be73cef98aa627a64b9184c4c3
chrisdev-ts <christian.serrano.puertos@gmail.com>
2025-08-03 18:35:27 -0600

    feat: implement login functionality with role-based redirection

--- src/services/auth/login.ts
+++ src/services/auth/login.ts
@@ -1,19 +1,18 @@
-// src/services/auth/login.ts
 import axios from "axios";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://ratacueva-api.onrender.com/api";
-
+const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "https://ratacueva-api.onrender.com/api";
 
 interface LoginPayload {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 
 export const login = async ({ email, password }: LoginPayload) => {
-    const response = await axios.post(`${API_URL}/auth/login`, {
-        email,
-        password,
-    });
+  const response = await axios.post(`${API_URL}/auth/login`, {
+    email,
+    password,
+  });
 
-    return response.data; 
+  return response.data;
 };
